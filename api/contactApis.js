4f3ce9be3624d53a5cadcd8ca7a7272433304dae
Abtik Digital <rbhensla1994@gmail.com>
2025-07-04 15:56:43 +0530

    updated Contact Api

--- api/contactApis.js
+++ api/contactApis.js
@@ -1,4 +1,4 @@
-import Joi, { required, types } from "joi";
+import Joi from "joi";
 import mongoose from "mongoose";
 import nodemailer from "nodemailer";
 
@@ -9,72 +9,72 @@ let contactModel;
 
 // Database Connection
 const handleDbConnection = async () => {
-  if (dbConnection) {
+    if (dbConnection) {
+        return dbConnection;
+    }
+    dbConnection = await mongoose.connect(MONGODB_CONNECTION_STRING);
     return dbConnection;
-  }
-  dbConnection = await mongoose.connect(MONGODB_CONNECTION_STRING);
-  return dbConnection;
 };
 
 // Send Mail
 const sendMail = async (from, to, subject, html) => {
-  try {
-    let transporter = nodemailer.createTransport({
-      auth: {
-        user: MAIL,
-        pass: MAIL_PASS,
-      },
-      service: "gmail",
-    });
-    let info = await transporter.sendMail({
-      from,
-      to,
-      subject,
-      html,
-    });
-    if (info) {
-      console.log("Mail Has Been Sent Successfully");
-    } else {
-      console.log("Error while Sending Mail");
+    try {
+        let transporter = nodemailer.createTransport({
+            auth: {
+                user: MAIL,
+                pass: MAIL_PASS,
+            },
+            service: "gmail",
+        });
+        let info = await transporter.sendMail({
+            from,
+            to,
+            subject,
+            html,
+        });
+        if (info) {
+            console.log("Mail Has Been Sent Successfully");
+        } else {
+            console.log("Error while Sending Mail");
+        }
+    } catch (error) {
+        console.log(`Error While Connecting Database`, error);
     }
-  } catch (error) {
-    console.log(`Error While Connecting Database`, error);
-  }
 };
 
 // Contact Model
 const contactSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "* Name Is Required"],
-      trim: true,
-      lowerCase: true,
+    {
+        name: {
+            type: String,
+            required: [true, "* Name Is Required"],
+            trim: true,
+            lowerCase: true,
+        },
+        email: {
+            type: String,
+            required: [true, "* Email Is Required"],
+            lowerCase: true,
+            trim: true,
+        },
+        message: {
+            type: String,
+            lowerCase: true,
+            trim: true,
+        },
     },
-    email: {
-      type: String,
-      required: [true, "* Email Is Required"],
-      lowerCase: true,
-      trim: true,
-    },
-    message: {
-      type: String,
-      lowerCase: true,
-      trim: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 contactModel =
-  mongoose.models.contactModel || mongoose.model("contactModel", contactSchema);
+    mongoose.models.contactModel || mongoose.model("contactModel", contactSchema);
 
 // Email Templates
 // (1) Firm Template
 const firmTemaplte = (data) => {
-  try {
-    let { name, email, message } = data;
+    try {
+        let { name, email, message } = data;
 
-    return `<!DOCTYPE html>
+        return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -162,13 +162,12 @@ const firmTemaplte = (data) => {
           <th>Email</th>
           <td>${email}</td>
         </tr>
-       ${
-         message &&
-         `<tr>
+       ${message &&
+            `<tr>
                 <th>Message</th>
                 <td>${message}</td>
             </tr>`
-       }
+            }
       </table>
     </div>
     <div class="footer">
@@ -178,16 +177,16 @@ const firmTemaplte = (data) => {
   </div>
 </body>
 </html>`;
-  } catch (error) {
-    console.log("Error While Creating Firm Template");
-  }
+    } catch (error) {
+        console.log("Error While Creating Firm Template");
+    }
 };
 
 // (2) User Template
 const userTemplate = (data) => {
-  try {
-    let { name, email, message } = data;
-    return `<!DOCTYPE html>
+    try {
+        let { name, email, message } = data;
+        return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -262,65 +261,68 @@ const userTemplate = (data) => {
 </body>
 </html>
 `;
-  } catch (error) {
-    console.log("Error While Generating User Template");
-  }
+    } catch (error) {
+        console.log("Error While Generating User Template");
+    }
 };
 
 // Validation Model
 const contactValidationSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+    }),
 });
 
 // Api Handler Function
 
 const handler = async (req, res) => {
-  try {
-    await handleDbConnection();
-    let { name, email, message } = req.body;
-    let { error } = contactValidationSchema.validate({ name, email });
-    if (error) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "Validation Error", error });
+    if (req.method !== "POST") {
+        return res.status(405).json({ isSuccess: false, message: "Only Post Method Is Allowed" })
     }
-    let isEmailAlreadyExist = await contactModel.findOne({ email });
-    if (isEmailAlreadyExist) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "Email Is Already Exist" });
+    try {
+        await handleDbConnection();
+        let { name, email, message } = req.body;
+        let { error } = contactValidationSchema.validate({ name, email });
+        if (error) {
+            return res
+                .status(400)
+                .json({ isSuccess: false, message: "Validation Error", error });
+        }
+        let isEmailAlreadyExist = await contactModel.findOne({ email });
+        if (isEmailAlreadyExist) {
+            return res
+                .status(400)
+                .json({ isSuccess: false, message: "Email Is Already Exist" });
+        }
+        let newContactDetails = new contactModel({
+            name,
+            email,
+            message,
+        });
+        let isSaved = await newContactDetails.save();
+        if (isSaved) {
+
+            await Promise.all([
+                await sendMail(MAIL, MAIL, firSubject, firmTemaplte(req.body)),
+                await sendMail(MAIL, email, userSubject, userTemplate(req.body)),
+            ]);
+            return res.status(201).json({
+                isSuccess: true,
+                message: "Contact Details Added Successfully",
+            });
+        } else {
+            return res.status(400).json({
+                isSuccess: false,
+                message: "Erro While Inserting Contact Details",
+            });
+        }
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ isSuccess: false, message: "Internal Server Error" });
     }
-    let newContactDetails = new contactModel({
-      name,
-      email,
-      message,
-    });
-    let isSaved = await newContactDetails.save();
-    if (isSaved) {
-        
-      await Promise.all([
-        await sendMail(MAIL, MAIL, firSubject, firmTemaplte(req.body)),
-        await sendMail(MAIL, email, userSubject, userTemplate(req.body)),
-      ]);
-      return res.status(201).json({
-        isSuccess: true,
-        message: "Contact Details Added Successfully",
-      });
-    } else {
-      return res.status(400).json({
-        isSuccess: false,
-        message: "Erro While Inserting Contact Details",
-      });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ isSuccess: false, message: "Internal Server Error" });
-  }
 };
 
 export default handler;
